e096990e173b631fb20ff25eda70953466973838
rm-park <somuch0321@gmail.com>
2023-02-26 22:14:00 +0900

    Merge for conflict.

--- app/src/main/java/com/dev/rmp/clockwidget/ClockWidget.kt
+++ app/src/main/java/com/dev/rmp/clockwidget/ClockWidget.kt
@@ -18,6 +18,8 @@ class ClockWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        Log.d(TAG, "onUpdate:")
+
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -28,14 +30,18 @@ class ClockWidget : AppWidgetProvider() {
      * 최초의 앱 위젯이 등록될 때 호출
      */
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is create
+        Log.d(TAG, "onEnabled:")
     }
 
     /**
      * 최종 앱 위젯 인스턴스가 삭제될 때 호출
      */
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        Log.d(TAG, "onDisabled:")
+    }
+
+    companion object {
+        private const val TAG = "ClockWidgetProvider"
     }
 
     /**
